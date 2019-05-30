67938a184d624cad1f82bb0a6ac9d423124b25b6
Maylen Medina <may@Maylens-MacBook-Pro.local>
2019-05-30 15:53:47 +1000

    gitpages added in json, structure components changed

---
+++ build/precache-manifest.6e0375e6f7e12b5a9b965816164263ba.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebde251ff3f6520b3384",
+    "url": "/portfolio/static/css/main.8634848e.chunk.css"
+  },
+  {
+    "revision": "ebde251ff3f6520b3384",
+    "url": "/portfolio/static/js/main.54cf5fe8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "9439f98f5e9152f69de6",
+    "url": "/portfolio/static/css/2.6e64dfe2.chunk.css"
+  },
+  {
+    "revision": "9439f98f5e9152f69de6",
+    "url": "/portfolio/static/js/2.4e0de6cb.chunk.js"
+  },
+  {
+    "revision": "b8c5b5663fd050c699e6ade07161e732",
+    "url": "/portfolio/static/media/screenshot_qube.b8c5b566.png"
+  },
+  {
+    "revision": "65532696249c02fd13fdd977dae14630",
+    "url": "/portfolio/static/media/screenshot_qube2.65532696.png"
+  },
+  {
+    "revision": "37511fb5ce52d20ddeebe6cb5117741c",
+    "url": "/portfolio/static/media/screenshot_qube3.37511fb5.png"
+  },
+  {
+    "revision": "fb916f45106ee4d267496297c7748452",
+    "url": "/portfolio/static/media/screenshot_qube4.fb916f45.png"
+  },
+  {
+    "revision": "1263367c1192eff704778fd16902d737",
+    "url": "/portfolio/static/media/logo_adv-studio.1263367c.png"
+  },
+  {
+    "revision": "d93989c23a95eb5e742356ac0d0136be",
+    "url": "/portfolio/static/media/screenshot_chat.d93989c2.png"
+  },
+  {
+    "revision": "d4cbab003d40ee56c357665df1fda340",
+    "url": "/portfolio/static/media/screenshot_chat2.d4cbab00.png"
+  },
+  {
+    "revision": "36144828be6d323af798c5bf5be87a97",
+    "url": "/portfolio/static/media/screenshot_todolist4.36144828.png"
+  },
+  {
+    "revision": "f8a6799b7c5c80376872dd2e202783f9",
+    "url": "/portfolio/static/media/logo_maylen.f8a6799b.png"
+  },
+  {
+    "revision": "9b817c048a15079517f7e663a193ff0e",
+    "url": "/portfolio/static/media/standing_girl.9b817c04.png"
+  },
+  {
+    "revision": "3c2c9556523f005fbc929654b1635450",
+    "url": "/portfolio/static/media/todo_gif.3c2c9556.gif"
+  },
+  {
+    "revision": "09eccd7cc4eb89fc45d46ccd61bce1bf",
+    "url": "/portfolio/static/media/living_1.09eccd7c.png"
+  },
+  {
+    "revision": "5d12fbff9af40ba8f2e054b6883a39df",
+    "url": "/portfolio/static/media/living_2.5d12fbff.png"
+  },
+  {
+    "revision": "63dde3adabb4e4cbf7036939f56b0565",
+    "url": "/portfolio/static/media/living_4.63dde3ad.png"
+  },
+  {
+    "revision": "75d793e75c937d8864cb131a80988b86",
+    "url": "/portfolio/static/media/living_3.75d793e7.png"
+  },
+  {
+    "revision": "fb3e3984d79ada87224e6ec117fa81db",
+    "url": "/portfolio/static/media/maylen_picture.fb3e3984.png"
+  },
+  {
+    "revision": "279196e37844ff702af163035a67c609",
+    "url": "/portfolio/static/media/right-arrow.279196e3.svg"
+  },
+  {
+    "revision": "17c001f0db08c978463b9d05ae5756b1",
+    "url": "/portfolio/static/media/keyboard.17c001f0.jpeg"
+  },
+  {
+    "revision": "cc44d59d1677a3fd17d4dd66a6bac423",
+    "url": "/portfolio/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import AboutMe from "./components/AboutMe";
 import Footer from "./components/Footer";
 import ParticlesComponent from "./components/ParticlesComponent";
 import ProjectsContainer from "./components/projectsContainer";
@@ -14,12 +16,14 @@ class App extends Component {
         <ScrollToTop>
           <ParticlesComponent />
           <Nav />
+          <Banner />
           <Switch>
             <Route
               path="/"
               render={() => <ProjectsContainer title="Some Projects" />}
             />
           </Switch>
+          <AboutMe />
           <Footer />
         </ScrollToTop>
       </Router>
