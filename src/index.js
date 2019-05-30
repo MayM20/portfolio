0c0f6a6cfaa46feaed3ff41490f70dd729e4c92d
Maylen Medina <may@Maylens-MacBook-Pro.local>
2019-05-30 15:09:52 +1000

    index edited

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css"; /*new added */
-import * as serviceWorker from "./serviceWorker"; /*what is this */
+import "bootstrap/dist/css/bootstrap.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("Maylen|Portfolio"));
 
